e1ef205d0e7ed033163af71545c3a010aff33467
Freddy Molleda <freddymolleda@MacBook-Pro-de-Freddy.local>
2025-08-21 21:06:58 -0400

    se quitaron credenciales

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -4,8 +4,7 @@ import { connect } from "mongoose";
 // const NODE_ENV = process.env.NODE_ENV;
 
 async function dbConnect(): Promise<void> {
-    //const DB_URI = <string>process.env.DB_URI;
-    const DB_URI = <string>'mongodb+srv://molledafreddy:magallanes2721.@cluster0.1e16p.mongodb.net/app-manager?retryWrites=true&w=majority&appName=Cluster0';
+    const DB_URI = <string>process.env.DB_URI;
     console.log(DB_URI);
     await connect(DB_URI)
 } 
