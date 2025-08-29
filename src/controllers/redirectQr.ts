533632c2ac5c6fd71f1955d510b03c894a47a784
Freddy Molleda <freddymolleda@MacBook-Pro-de-Freddy.local>
2025-08-28 23:27:53 -0400

    se agrego el numero requerido

--- src/controllers/redirectQr.ts
+++ src/controllers/redirectQr.ts
@@ -6,7 +6,7 @@ import { handleHttp } from "../utils/error.handle";
 
 const getRedirectQr = async ({params}: Request, res: Response) => {
   try {
-      let numero = "56953941370"; // Número de teléfono del destinatario
+      let numero = "56979643935"; // Número de teléfono del destinatario
       let apiWhatsapp = `https://wa.me/${numero}?text=Me%20gustaria%20Realizar%20un%20Pedido`;
 
       // Responde al cliente con el enlace
